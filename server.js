85a1e0c324b81c8289842f35f714d26740b676de
yogitha-tech <Yogitharamanathan26@gmail.com>
2026-07-29 15:55:53 +0530

    Change static file serving directory to root

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve the frontend
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // API routes
 app.use('/api/tasks', tasksRouter);
