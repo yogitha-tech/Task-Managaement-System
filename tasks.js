92ae3ca6c9d992a834ce54b22b9ced766e636657
yogitha-tech <Yogitharamanathan26@gmail.com>
2026-07-29 15:57:20 +0530

    Change database path in tasks.js

--- tasks.js
+++ tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('./db');
 
 const VALID_PRIORITIES = ['LOW', 'MEDIUM', 'HIGH'];
 const VALID_STATUSES = ['TODO', 'IN_PROGRESS', 'DONE'];
